f833168ce30aef24990a42367879cd979cd3c518
noyonalways <noyonrahman2003@gmail.com>
2024-02-03 01:21:20 +0600

    feat: List up the endpoints

---
+++ src/routes/index.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+const router = Router();
+
+export default router;
+
+/**
+ * User Endpoints
+ * - POST /api/users
+ * - GET /api/users
+ * - GET /api/users/:userId
+ * - PUT /api/users/:userId
+ * - DELETE /api/users/:userId
+ *
+ * Order Endpoints
+ * - PUT /api/users/:userId/orders
+ * - GET /api/users/:userId/orders
+ * - GET /api/users?:userId/total-price
+ */
