6ad2460a290d25316e916a3f1a21717de97547fe
noyonalways <noyonrahman2003@gmail.com>
2024-02-06 00:13:47 +0600

    feat: User validation updated

--- src/validation/users.ts
+++ src/validation/users.ts
@@ -3,16 +3,16 @@ import { z } from "zod";
 export const UserValidationSchema = z.object({
   userId: z.string().optional(),
   username: z
-    .string()
-    .min(1, "Username is required and minimum length 1")
-    .max(30),
-  password: z.string().min(6),
+    .string({ required_error: "Username is required" })
+    .min(1, "Username minimum length 1")
+    .max(30, "Username is too long"),
+  password: z.string({ required_error: "Password is required" }).min(6),
   fullName: z.object({
     firstName: z
-      .string()
-      .min(1, "First Name is required and min length 1")
-      .max(30),
-    lastName: z.string().max(30).optional(),
+      .string({ required_error: "First name is required" })
+      .min(1, "First Name min length 1")
+      .max(30, "First name is to long"),
+    lastName: z.string().max(30, "Last name is too long").optional(),
   }),
   address: z
     .object({
@@ -22,7 +22,9 @@ export const UserValidationSchema = z.object({
     })
     .optional(),
   age: z.number(),
-  email: z.string().email(),
+  email: z
+    .string({ required_error: "Email is required" })
+    .email("Provide a valid email"),
   isActive: z.boolean().optional(),
   hobbies: z.array(z.string().max(30)).optional(),
 });
@@ -30,12 +32,12 @@ export const UserValidationSchema = z.object({
 export type UserValidationProperties = z.infer<typeof UserValidationSchema>;
 
 export const UpdateUserValidationSchema = z.object({
-  username: z.string().optional(),
+  username: z.string().max(30, "Username is too long").optional(),
   password: z.string().optional(),
   fullName: z
     .object({
-      firstName: z.string().optional(),
-      lastName: z.string().optional(),
+      firstName: z.string().max(30, "First name is to long").optional(),
+      lastName: z.string().max(30, "Last name is to long").optional(),
     })
     .optional(),
   address: z
